4c3f76ac78e9c0d494a9cd07468a9adb95c70a34
Omar Alhaddar <omaralhaddar11@gmail.com>
2021-05-21 15:53:56 +0300

    flask checkerboard

--- flask/flask_fundamentals/Checkerboard/Checkerboard.py
+++ flask/flask_fundamentals/Checkerboard/Checkerboard.py
@@ -3,7 +3,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html",num=int())
+
+    return render_template("index.html",row=8,col=8)
+
+@app.route('/<x>')
+def index4(x):
+
+    return render_template("index.html",row=int(x),col=8)
+
+@app.route("/<x>/<y>")
+def play(x,y):
+
+    return render_template("index.html",row=int(x),col=int(y))
+
+@app.route("/<x>/<y>/<color1>/<color2>")
+def colors(x,y,color1,color2):
+    
+    return render_template("index.html",row=int(x),col=int(y),color1=(color1),color2=(color2))
 
 if __name__=="__main__":
     app.run(debug=True)
