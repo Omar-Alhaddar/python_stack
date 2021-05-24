e683f25ac71e53a87bd5d0ac7d85b9c6d0d172fe
Omar Alhaddar <omaralhaddar11@gmail.com>
2021-05-24 22:03:22 +0300

    orm finish

--- django/django_orm/BooksAuthors (Shell)/books_authors_proj/books_authors_app/urls.py
+++ django/django_orm/BooksAuthors (Shell)/books_authors_proj/books_authors_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.book),
+    path('add_book', views.add_book),
+    path('authors', views.author),
+    path('add_author', views.add_author),
+    path('books/<int:id>', views.books_details),
+    path('authors/<int:id>', views.authors_details),
+    path('add_auth/<int:id>', views.add_author),
+    path('add_newbook/<int:id>', views.add_book),
 ]
